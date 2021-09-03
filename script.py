9bbee99cbd78ff963197435437f2eb6e886e58f4
Ndiaye HANN <cawral@hotmail.fr>
2021-09-03 12:04:39 +0200

    Chargement des urls des pages produit de la catégorie mystery

--- script.py
+++ script.py
@@ -1,9 +1,14 @@
-from functions import get_book_page
+from functions import get_book_page, get_book_urls
 import csv  
-
+"""
 book_info = get_book_page('https://books.toscrape.com/catalogue/lost-among-the-living_31/index.html') 
 print(book_info)
 
 with open('book_page_info.csv', 'w', encoding = 'utf-8-sig') as csv_file:
     writer = csv.writer(csv_file)
     writer.writerows([book_info])    
+"""
+
+for i in range(1,3):
+    book_urls = get_book_urls(f'https://books.toscrape.com/catalogue/category/books/mystery_3/page-{i}.html')
+    print(book_urls)
